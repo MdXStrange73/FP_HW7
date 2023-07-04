f7280433325ae4a8ef3e4f8c17f1cca6b2960a1a
Yavorskiy Vlad <yavorski.vlad98@gmail.com>
2023-07-04 11:17:16 +0300

    Last commit

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 const QUESTIONS = [
-    {
-      question: 'Какое количество игроков составляет команду в "League of Legends"?',
-      answer: '5',
-      type: 'prompt',
-    },
-    {
-      question: 'Какая позиция в "League of Legends" отвечает за контроль над картой?',
-      answer: 'Джанглер',
-      type: 'prompt',
-    },
-    {
-      question: 'Какая роль в "League of Legends" обычно играет в линии верхнего коридора?',
-      answer: 'Топ',
-      type: 'prompt',
-    },
-    {
-      question: 'Какое существо появляется в "League of Legends" через определенные промежутки времени и дает улучшения команде, которая его убивает?',
-      answer: 'Дракон',
-      type: 'prompt',
-    },
-    {
-      question: 'Какая роль в "League of Legends" обычно играет в линии среднего коридора?',
-      answer: 'Мид',
-      type: 'prompt',
-    },
-    {
-      question: 'Какой чемпион в "League of Legends" известен своим глобальным ультимейтом, позволяющим телепортироваться на другую часть карты?',
-      answer: 'Твистед Фейт',
-      type: 'prompt',
-    },
-    {
-      question: 'Какая роль в "League of Legends" обычно играет в линии нижнего коридора в паре с поддержкой?',
-      answer: 'ADC',
-      type: 'prompt',
-    },
-    {
-      question: 'Какой чемпион в "League of Legends" известен своим мощным контролем толпы и способностью создавать стены?',
-      answer: 'Анивия',
-      type: 'prompt',
-    },
-    {
-      question: 'Какое событие в "League of Legends" проводится ежегодно и собирает лучших профессиональных игроков со всего мира?',
-      answer: 'Worlds',
-      type: 'prompt',
-    },
-    {
-      question: 'Какой чемпион в "League of Legends" известен своей способностью прыгать на врагов и проводить комбо из ударов?',
-      answer: 'Ривен',
-      type: 'prompt',
-    },
-  ];
-  
-  let score = 0;
-  
-  for (let i = 0; i < QUESTIONS.length; i++) {
-    const question = QUESTIONS[i];
-    let userAnswer;
-  
-    if (question.type === 'prompt') {
-      userAnswer = prompt(question.question);
-    } else if (question.type === 'confirm') {
-      userAnswer = confirm(question.question);
-    }
-  
-    if (userAnswer === question.answer) {
-      score += 10;
-    }
+  {
+    question: 'Команда в "League of Legends" состоит из 5 игроков. Подтвердите это.',
+    answer: true,
+    type: 'confirm',
+  },
+  {
+    question: 'Джанглер в "League of Legends" отвечает за контроль над картой. Подтвердите это.',
+    answer: true,
+    type: 'confirm',
+  },
+  {
+    question: 'В линии верхнего коридора играет роль "Топ". Подтвердите это.',
+    answer: true,
+    type: 'confirm',
+  },
+  {
+    question: 'Дракон в "League of Legends" дает улучшения команде, которая его убивает. Подтвердите это.',
+    answer: true,
+    type: 'confirm',
+  },
+  {
+    question: 'Мид - это роль, которая обычно играет в линии среднего коридора. Подтвердите это.',
+    answer: true,
+    type: 'confirm',
+  },
+  {
+    question: 'Какой чемпион в "League of Legends" известен своим глобальным ультимейтом, позволяющим телепортироваться на другую часть карты?',
+    answer: 'Твистед Фейт',
+    type: 'prompt',
+  },
+  {
+    question: 'В линии нижнего коридора играет роль "ADC" в паре с поддержкой. Верно или нет?',
+    answer: false,
+    type: 'confirm',
+  },
+  {
+    question: 'Какой чемпион в "League of Legends" известен своим мощным контролем толпы и способностью создавать стены?',
+    answer: 'Анивия',
+    type: 'prompt',
+  },
+  {
+    question: 'Событие "Worlds" проводится ежегодно и собирает лучших профессиональных игроков со всего мира. Подтвердите это.',
+    answer: true,
+    type: 'confirm',
+  },
+  {
+    question: 'Какой чемпион в "League of Legends" известен своей способностью прыгать на врагов и проводить комбо из ударов?',
+    answer: 'Ривен',
+    type: 'prompt',
+  },
+];
+
+let score = 0;
+
+for (let i = 0; i < QUESTIONS.length; i++) {
+  const question = QUESTIONS[i];
+  let userAnswer;
+
+  if (question.type === 'prompt') {
+    userAnswer = prompt(question.question);
+  } else if (question.type === 'confirm') {
+    userAnswer = confirm(question.question);
   }
-  
-  alert(`Ваш результат: ${score} очков`);
+
+  if (userAnswer === question.answer) {
+    score += 10;
+  }
+}
+
+alert(`Ваш результат: ${score} очков`);
